28b5eee74415cb62bdf6954e8257f9e184b1d5e8
Frédéric Vauchelles <frederic.vauchelles@gmail.com>
2017-11-17 17:05:48 +0100

    wip BC6H Gpu

---
+++ SampleScenes/Common/Scripts/CompressBC6HAndDisplay.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Experimental.Rendering.HDPipeline;
+using UnityEngine.Rendering;
+
+[ExecuteInEditMode]
+public class CompressBC6HAndDisplay : MonoBehaviour
+{
+    [SerializeField]
+    ComputeShader m_BC6HShader;
+    [SerializeField]
+    Texture m_SourceTexture;
+    [SerializeField]
+    Material m_SourceMaterial;
+    [SerializeField]
+    string m_TextureName = "_ColorMap";
+
+    int m_Hash = 0;
+    RenderTargetIdentifier m_SourceId;
+    Texture m_Target;
+    RenderTargetIdentifier m_TargetId;
+
+    Renderer m_Renderer;
+    Material m_Material;
+    BC6H m_BC6H;
+
+    void OnEnable()
+    {
+        if (m_Material != null)
+            DestroyImmediate(m_Material);
+
+        m_Material = Instantiate(m_SourceMaterial);
+        var renderer = m_Renderer ?? (m_Renderer = GetComponent<Renderer>());
+        renderer.material = m_Material;
+
+        m_BC6H = new BC6H(m_BC6HShader);
+
+        HDRenderPipeline.OnPreRender += OnPreRender;
+    }
+
+    void OnPreRender(CommandBuffer cmb)
+    {
+        if (m_SourceTexture == null
+            || m_SourceMaterial == null
+            || m_BC6HShader == null)
+        {
+            enabled = false;
+            return;
+        }
+
+        using (new ProfilingSample(cmb, "BC6H Test"))
+        {
+            var hash = CalculateHash(m_SourceTexture);
+            if (m_Hash != hash)
+            {
+                m_Hash = hash;
+                m_SourceId = new RenderTargetIdentifier(m_SourceTexture);
+                CreateTargetInstance();
+            }
+
+            m_BC6H.EncodeFastCubemap(cmb, m_SourceId, m_SourceTexture.width, m_TargetId);
+        }
+
+        m_Material.SetTexture(m_TextureName, m_Target);
+    }
+
+    void OnDisable()
+    {
+        HDRenderPipeline.OnPreRender -= OnPreRender;
+    }
+
+    [ContextMenu("Create Target")]
+    void CreateTargetInstance()
+    {
+        if (m_Target is RenderTexture)
+            ((RenderTexture)m_Target).Release();
+
+        var t = new Cubemap(m_SourceTexture.width, TextureFormat.BC6H, false);
+        m_Target = t;
+
+        m_Material.SetTexture(m_TextureName, m_Target);
+        m_SourceId = new RenderTargetIdentifier(m_SourceTexture);
+        m_TargetId = new RenderTargetIdentifier(m_Target);
+    }
+
+    static int CalculateHash(Texture texture)
+    {
+        return texture.width ^ texture.height ^ texture.GetInstanceID();
+    }
+}
---
+++ ScriptableRenderPipeline/Core/BC6H.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine.Assertions;
+using UnityEngine.Rendering;
+
+namespace UnityEngine.Experimental.Rendering
+{
+    public class BC6H
+    {
+        public static BC6H DefaultInstance;
+
+        static readonly int _Source = Shader.PropertyToID("_Source");
+        static readonly int _Target = Shader.PropertyToID("_Target");
+        static readonly int __Tmp_RT0 = Shader.PropertyToID("__Tmp_RT0");
+        static readonly int __Tmp_RT1 = Shader.PropertyToID("__Tmp_RT1");
+
+        static readonly RenderTextureDescriptor k_CubemapToRTArrayDescriptor = new RenderTextureDescriptor
+        {
+            autoGenerateMips = false,
+            bindMS = false,
+            colorFormat = RenderTextureFormat.ARGBHalf,
+            depthBufferBits = 0,
+            dimension = TextureDimension.Tex2DArray,
+            enableRandomWrite = true,
+            sRGB = false,
+            volumeDepth = 6,
+            useMipMap = false,
+            msaaSamples = 1
+        };
+
+        readonly ComputeShader m_Shader;
+        readonly int m_KernelEncodeFast;
+        readonly int[] m_KernelEncodeFastGroupSize;
+
+        public BC6H(ComputeShader shader)
+        {
+            Assert.IsNotNull(shader);
+
+            m_Shader = shader;
+            m_KernelEncodeFast = m_Shader.FindKernel("KEncodeFastCubemap");
+
+            uint x, y, z;
+            m_Shader.GetKernelThreadGroupSizes(m_KernelEncodeFast, out x, out y, out z);
+            m_KernelEncodeFastGroupSize = new[] { (int)x, (int)y, (int)z };
+        }
+
+        // Only use mode11 of BC6H encoding
+        public void EncodeFastCubemap(CommandBuffer cmb, RenderTargetIdentifier source, int sourceSize, RenderTargetIdentifier target)
+        {
+            int targetWidth, targetHeight;
+            CalculateOutputSize(sourceSize, sourceSize, out targetWidth, out targetHeight);
+
+            // Convert TextureCube source to Texture2DArray
+            var cubemapToTexture2DArrayDescriptor = k_CubemapToRTArrayDescriptor;
+            cubemapToTexture2DArrayDescriptor.width = sourceSize;
+            cubemapToTexture2DArrayDescriptor.height = sourceSize;
+            cmb.GetTemporaryRT(__Tmp_RT0, cubemapToTexture2DArrayDescriptor);
+
+            for (var i = 0; i < 6; i++)
+                cmb.CopyTexture(source, i, __Tmp_RT0, i);
+
+            // Allocate temporary target
+            cubemapToTexture2DArrayDescriptor.colorFormat = RenderTextureFormat.ARGBInt;
+            cubemapToTexture2DArrayDescriptor.width = targetWidth;
+            cubemapToTexture2DArrayDescriptor.height = targetHeight;
+            cmb.GetTemporaryRT(__Tmp_RT1, cubemapToTexture2DArrayDescriptor);
+
+            cmb.SetComputeTextureParam(m_Shader, m_KernelEncodeFast, _Source, __Tmp_RT0);
+            cmb.SetComputeTextureParam(m_Shader, m_KernelEncodeFast, _Target, __Tmp_RT1);
+            cmb.DispatchCompute(m_Shader, m_KernelEncodeFast, targetWidth, targetHeight, 6);
+
+            // Convert target Texture2DArray to TextureCube
+            for (var i = 0; i < 6; i++)
+                cmb.CopyTexture(__Tmp_RT1, i, target, i);
+
+            cmb.ReleaseTemporaryRT(__Tmp_RT0);
+            cmb.ReleaseTemporaryRT(__Tmp_RT1);
+        }
+
+        static void CalculateOutputSize(int swidth, int sheight, out int twidth, out int theight)
+        {
+            // BC6H encode 4x4 blocks of 32bit in 128bit
+            twidth = swidth >> 2;
+            theight = sheight >> 2;
+        }
+    }
+
+    public static class BC6HExtensions
+    {
+        public static void BC6HEncodeFastCubemap(this CommandBuffer cmb, RenderTargetIdentifier source, int sourceSize, RenderTargetIdentifier target)
+        {
+            BC6H.DefaultInstance.EncodeFastCubemap(cmb, source, sourceSize, target);
+        }
+    }
+}
